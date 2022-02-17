f539bd467cf853f7dfa91018e8e47003e894adb4
cdedonno <carlo.dedonno@helmholtz-muenchen.de>
2022-02-17 18:11:21 +0100

    remove own gaussian nll

--- cpa/model.py
+++ cpa/model.py
@@ -46,30 +46,6 @@ class NBLoss(torch.nn.Module):
 def _nan2inf(x):
     return torch.where(torch.isnan(x), torch.zeros_like(x) + np.inf, x)
 
-class GaussianLoss(torch.nn.Module):
-    """
-    Gaussian negative log-likelihood loss. It assumes targets `y` with n rows and d
-    columns, but estimates `yhat` with n rows and 2d columns. The columns 0:d
-    of `yhat` contain estimated means, the columns d:2*d of `yhat` contain
-    estimated variances. This module assumes that the estimated variances are
-    positive---for numerical stability, it is recommended that the minimum
-    estimated variance is greater than a small number (1e-3).
-    """
-
-    def __init__(self):
-        super(GaussianLoss, self).__init__()
-
-    def forward(self, yhat, y):
-        dim = yhat.size(1) // 2
-        mean = yhat[:, :dim]
-        variance = yhat[:, dim:]
-
-        term1 = variance.log().div(2)
-        term2 = (y - mean).pow(2).div(variance.mul(2))
-
-        return (term1 + term2).mean()
-
-
 class MLP(torch.nn.Module):
     """
     A multilayer perceptron with ReLU activations and optional BatchNorm.
